af32925cfbd6176f1dca914ff10c073092ea4299
Aktyr <voiloshnikov02@mail.ru>
2026-03-18 22:59:03 +0300

    Added authentication packages

--- Application/Globals.cs
+++ Application/Globals.cs
@@ -5,7 +5,6 @@ global using LibApp.Core.DTO.Borrowing;
 global using LibApp.Core.DTO.Entities;
 global using LibApp.Core.Entities;
 global using LibApp.Core.Exceptions;
-global using LibApp.Core.Exceptions.Borrowing;
 global using LibApp.Core.Exceptions.Entities;
 global using LibApp.Core.Interfaces;
 global using LibApp.Core.Records;
